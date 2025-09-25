e848e5d27b25f35371d3724d7ec9688ce89cf415
Alex <alexcarrerav2@gmail.com>
2025-09-25 15:17:48 +0200

    added docstrings

--- graph.py
+++ graph.py
@@ -8,10 +8,20 @@ Created on Thu Sep 25 14:13:37 2025
 
 
 def ajouter_noeud(graphe, noeud):
+    """
+    graphe: graphe a modifier
+    noeud: noeud a ajouter
+    """
+
     if (noeud in graphe) == 0 : 
         graphe[noeud] = []
 
 def ajouter_arete(graphe, noeud, arete):
+    """
+    graphe: graphe a modifier
+    noeud: noeud auquel nous allons ajouter l'arete
+    arete: arete que nous allons ajouter
+    """
     if noeud in graphe:
         if (arete in graphe[noeud]) == 0:
             graphe[noeud].append(arete)
